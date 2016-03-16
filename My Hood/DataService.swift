dcb0e7ad9b72fb4b133e2d19f30973f77561aa03
Ishan Shams <ishanshams@Ishans-MBP.byod.gmu.edu>
2016-03-15 22:11:59 -0400

    done my hood app

---
+++ My Hood/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  My Hood
+//
+//  Created by Ishan Shams on 3/15/16.
+//  Copyright © 2016 Ishan Shams. All rights reserved.
+//
+
+import Foundation
---
+++ My Hood/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  My Hood
+//
+//  Created by Ishan Shams on 3/15/16.
+//  Copyright © 2016 Ishan Shams. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
